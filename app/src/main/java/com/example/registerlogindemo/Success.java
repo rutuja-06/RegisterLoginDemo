324af54b442054b88b6134b4c1a67c454542c0d1
rutuja-06 <rutujajogdand06@gmail.com>
2021-10-29 09:14:48 +0530

    Reg Form

---
+++ app/src/main/java/com/example/registerlogindemo/Success.java
@@ -0,0 +1,16 @@
+package com.example.registerlogindemo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.registerlogindemo.R;
+
+public class Success extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.success);
+    }
+}
